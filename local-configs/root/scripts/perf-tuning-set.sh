e32a9a985c1346f8f52d9b239e3e88da5560c78f
Steven Han <s.han.1@elsevier.com>
2016-12-19 21:49:40 -0500

    update

--- local-configs/root/scripts/perf-tuning-set.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo acpi_pm > /sys/devices/system/clocksource/clocksource0/current_clocksource
-echo never > /sys/kernel/mm/transparent_hugepage/enabled
-echo never > /sys/kernel/mm/transparent_hugepage/defrag
-
-
-
-
