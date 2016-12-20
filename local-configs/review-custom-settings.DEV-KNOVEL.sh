7692dc56980f909a90b67358188a666d97b43284
Steven Han <s.han.1@elsevier.com>
2016-12-20 14:15:52 -0500

    update

--- local-configs/review-custom-settings.DEV-KNOVEL.sh
+++ local-configs/review-custom-settings.DEV-KNOVEL.sh
@@ -23,26 +23,26 @@ cat /sys/kernel/mm/transparent_hugepage/defrag
 echo
 echo
 
-echo "/sys/block/sda/queue/scheduler: "
-cat /sys/block/sda/queue/scheduler
+echo "/sys/block/xvda/queue/scheduler: "
+cat /sys/block/xvda/queue/scheduler
 
 echo
 echo
 
-echo "/sys/block/sda/queue/rq_affinity: "
-cat /sys/block/sda/queue/rq_affinity
+echo "/sys/block/xvda/queue/rq_affinity: "
+cat /sys/block/xvda/queue/rq_affinity
 
 echo
 echo
 
-echo "/sys/block/sda/queue/nr_requests: "
-cat /sys/block/sda/queue/nr_requests
+echo "/sys/block/xvda/queue/nr_requests: "
+cat /sys/block/xvda/queue/nr_requests
 
 echo
 echo
 
-echo "/sys/block/sda/queue/read_ahead_kb: "
-cat /sys/block/sda/queue/read_ahead_kb
+echo "/sys/block/xvda/queue/read_ahead_kb: "
+cat /sys/block/xvda/queue/read_ahead_kb
 
 echo
 echo
