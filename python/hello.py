6b0a113a0393a8e65c5b25c530324ddd28e22930
Steven Han <s.han.1@elsevier.com>
2016-12-21 17:46:37 -0500

    initial commit

---
+++ python/hello.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+MSG = '''
+INTRO TO PYTHON
+First script - hello world :-)
+'''
+
+
+print MSG
+
+
+print "\t1...\n"; print "\t2...\n"; print "\t3...\n";
+
+
+print "{name} wrote this \"{file}\"".format(name="Woody",file="hello.py")
+
+
+
